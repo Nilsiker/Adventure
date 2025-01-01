0dd8a79f1f5ec1cf3d47a2d9908c9f603285acad
Andreas Nilsson <nilsiker94@gmail.com>
2025-01-01 12:28:44 +0100

    wip: minor game node restructure

--- src/tilemap/Ground.cs
+++ src/tilemap/Ground.cs
@@ -9,4 +9,25 @@ public partial class Ground : TileMapLayer
 
   // Called every frame. 'delta' is the elapsed time since the previous frame.
   public override void _Process(double delta) { }
+
+  public override void _UnhandledInput(InputEvent @event)
+  {
+    if (@event is InputEventMouseMotion motion)
+    {
+      // var clicked_cell = tile_map_layer.local_to_map(tile_map_layer.get_local_mouse_position())
+      // var data = tile_map_layer.get_cell_tile_data(clicked_cell)
+      // if data:
+      //     return data.get_custom_data("power")
+      // else:
+      //     return 0
+      var cellPos = LocalToMap(GetLocalMousePosition());
+      var data = GetCellTileData(cellPos);
+      if (data == null)
+      {
+        return;
+      }
+
+      GD.Print(data.Terrain);
+    }
+  }
 }
