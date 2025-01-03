fefee4da14856a26d019d51bf94e96e46a0e96e3
Andreas Nilsson <nilsiker94@gmail.com>
2025-01-03 13:49:39 +0100

    wip

--- src/world/tree/state/TreeLogic.Output.cs
+++ src/world/tree/state/TreeLogic.Output.cs
@@ -8,7 +8,7 @@ public partial class TreeLogic
   {
     public record struct UpdateSprite(Texture2D Texture); // TODO replace with stage enum to decouple from Godot?
 
-    public record struct Rustle(float Strength); // TODO replace with stage enum to decouple from Godot?
+    public record struct Rustle(float Strength);
 
     public record struct ProduceWood(int Amount); // TODO replace with Produce/Drop Item method, passing item data?
   }
